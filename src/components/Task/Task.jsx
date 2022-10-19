8cb41795f0f0e85925348a553684ebc8fce338f3
hafsa <hfsyln93@gmail.com>
2022-10-20 00:53:24 +0300

    edit

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -2,36 +2,67 @@ import { useState } from "react";
 import AddTask from "../AddTask";
 
 const Task = () => {
-  const [showTask, setShowTask] = useState("")
-  const [showDate, setShowDate] = useState("")
+
+  const [task, setTitle] = useState("")
+  const [date, setDate] = useState("")
+  const [sendTask, setSendTask] = useState("")
 
   
-  const changeValue = () => {
-   document.querySelector(".div-none").classList.add("div-none");
-   document.querySelector(".div-flex").classList.remove("div-flex");
-  }
+  const handleSubmit = (e) => {
+    // form içerisindeki buton submit olur ve handlesubmit otomatik çalışır
+    e.preventDefault();
+    const newTask = { task, date}; //aynı isim ile gönderiyorsam yeterli ama t:title de olur
+    console.log(newTask)
+    setSendTask([newTask])
+    console.log(sendTask)
+    console.log(...sendTask)
+    setTitle("");
+    setDate("");
+  };
+
+
+
+
   return (
-    <div className="taskEdit">
-          <div className="entered">
-            <label htmlFor="">Task</label>
-            <input type="text"   
-            value={showTask}
-            onChange={(e) => {setShowTask(e.target.value);}}/>
-            
-            <label htmlFor="">Day & time</label>
-            <input type="text"  
-              value={showDate}
-              onChange={(e) => {setShowDate(e.target.value);}}/>
-            
-            <button onClick={changeValue}>Save Task</button>
-          </div>
-          <div className="div-flex" >
-            
-          <p>fgb</p>
-          <AddTask  task = {showTask}  date = {showDate} />
-          </div>
-    </div>
+    <div className="container text-center mt-4">
+    <h1 className="display-6 text-danger">Add Your Tutorial</h1>
+    <form onSubmit={handleSubmit}>
+      <div className="mb-3">
+        <label htmlFor="title" className="form-label">
+          Task
+        </label>
+        <input
+          type="text"
+          className="form-control"
+          id="title"
+          placeholder="Enter your title"
+          value={task}
+          onChange={(e) => setTitle(e.target.value)}
+          required
+        />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="desc" className="form-label">
+          Date
+        </label>
+        <input
+          type="text"
+          className="form-control"
+          id="desc"
+          placeholder="Enter your Description"
+          value={date}
+          onChange={(e) => setDate(e.target.value)}
+          required
+        />
+      </div>
+      <button className="btn btn-danger mb-4">Submit</button> 
+      
+    </form>
+
+    <AddTask sendTask = {...sendTask} />
+  </div>
+ 
   )
 }
 
-export default Task
+export default Task;
