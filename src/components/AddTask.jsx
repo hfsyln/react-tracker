3b86aae3e0a540219f36d29801b98bf95a894c0f
hafsa <hfsyln93@gmail.com>
2022-10-20 00:53:40 +0300

    edit

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,8 +1,12 @@
 import Task from "./Task/Task"
 
-const AddTask = ({task, date}) => {
+const AddTask = (sendTask) => {
+  console.log(sendTask)
+  const {task, date} = sendTask
+  console.log(task)
+  console.log(date)
   return (
-    <div>
+    <div className="card">
       <h1>{task}</h1>
       <p>{date}</p>
     </div>
